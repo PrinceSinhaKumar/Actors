852968ce1f3a77218b0eef758a7c250a3f8d9bdc
Prince Sinha <prince.kumar042@gmail.com>
2024-08-25 19:36:47 +0530

    Data Race Condition: Fix using DispatchBarrier

--- Actors in swift/Actors in swift/ViewModel/TicketSystemViewModel.swift
+++ Actors in swift/Actors in swift/ViewModel/TicketSystemViewModel.swift
@@ -12,12 +12,19 @@ class TicketSystemViewModel {
     private let companyName: String = "Indigo airlines"
     private var availableTickets: [String] = ["1A","2A","1B","2B","1C","2C"]
     
+    private let barrier = DispatchQueue(label: "barrier")
+    
     func getAllTickets() -> [String] {
-        availableTickets
+        barrier.sync(flags: .barrier) {
+            availableTickets
+        }
     }
     func bookedTicket() -> String {
-        let booked = availableTickets.first ?? ""
-        availableTickets.removeFirst()
-        return booked
+        barrier.sync(flags: .barrier) {
+            let booked = availableTickets.first ?? ""
+            availableTickets.removeFirst()
+            return booked
+        }
+        
     }
 }
